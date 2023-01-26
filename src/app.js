4009156059c5e071048dd8f45f34348bdf3f7075
verebélyi ivett <ivett.verebelyi@gmail.com>
2023-01-26 19:31:45 +0100

    Webes UI kész

---
+++ src/app.js
@@ -0,0 +1,9 @@
+/*
+* File:app.js
+* Author: Verebélyi Ivett
+* Copyright: 2023, Verebélyi Ivett
+* Group: Szoft I-2  E
+* Date: 2023-01-26
+* Github: https://github.com/IvettAdel/Trapter
+* Licenc: GNU GPL
+*/
